9e433a86874d54a27db231ce051d5d1b39f300bf
Loïc Bartoletti <lbartoletti@FreeBSD.org>
2022-01-05 21:44:40 +0100

    textproc/sigil: fix build against minizip-ng
    Use internal minizip.

---
+++ textproc/sigil/files/patch-3rdparty_minizip_ioapi.c
@@ -0,0 +1,29 @@
+--- 3rdparty/minizip/ioapi.c.orig	2022-01-05 19:26:08 UTC
++++ 3rdparty/minizip/ioapi.c
+@@ -112,7 +112,7 @@ static voidpf ZCALLBACK fopen64_file_func (voidpf opaq
+         mode_fopen = "wb";
+ 
+     if ((filename!=NULL) && (mode_fopen != NULL))
+-        file = fopen64((const char*)filename, mode_fopen);
++        file = fopen((const char*)filename, mode_fopen);
+     return file;
+ }
+ 
+@@ -142,7 +142,7 @@ static long ZCALLBACK ftell_file_func (voidpf opaque, 
+ static ZPOS64_T ZCALLBACK ftell64_file_func (voidpf opaque, voidpf stream)
+ {
+     ZPOS64_T ret;
+-    ret = ftello64((FILE *)stream);
++    ret = ftello((FILE *)stream);
+     return ret;
+ }
+ 
+@@ -188,7 +188,7 @@ static long ZCALLBACK fseek64_file_func (voidpf  opaqu
+     }
+     ret = 0;
+ 
+-    if(fseeko64((FILE *)stream, offset, fseek_origin) != 0)
++    if(fseeko((FILE *)stream, offset, fseek_origin) != 0)
+                         ret = -1;
+ 
+     return ret;
