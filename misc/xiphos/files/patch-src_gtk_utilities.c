6933ba3495faf912d13c918098baff2bdc670ec6
Loïc Bartoletti <lbartoletti@FreeBSD.org>
2022-01-05 21:48:06 +0100

    misc/xiphos: fix build against minizip-ng
    Use internal minizip.

---
+++ misc/xiphos/files/patch-src_gtk_utilities.c
@@ -0,0 +1,11 @@
+--- src/gtk/utilities.c.orig	2022-01-05 16:53:00 UTC
++++ src/gtk/utilities.c
+@@ -38,7 +38,7 @@
+ #include <gdk-pixbuf/gdk-pixbuf.h>
+ 
+ #include <gtk/gtk.h>
+-#include <zip.h>
++#include <minizip/zip.h>
+ #include <zlib.h>
+ 
+ #include "gui/utilities.h"
